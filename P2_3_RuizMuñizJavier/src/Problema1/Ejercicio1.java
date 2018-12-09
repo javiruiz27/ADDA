360a417f0da7a8e1eb4c36a39cae5a530062de5a
Javier Ruiz <javiruizz.jr@gmail.com>
2018-12-09 15:41:09 +0100

    Ejercicio1

---
+++ P2_3_RuizMuñizJavier/src/Problema1/Ejercicio1.java
@@ -0,0 +1,19 @@
+package Problema1;
+
+public class Ejercicio1 {
+
+	/* Decidir si un árbol binario es equilibrado */
+	/*
+	 * Un árbol equilibrado es un árbol binario de búsqueda en el que para cada nodo el número de niveles
+	 * de sus subárboles izquierdo y derecho no debe diferir en más de una unidad.
+	 */
+	
+	//1.- Crear un árbol binario de búsqueda.
+	/*Sea h el número de niveles, la condición que se debe cumplir para que sea equilibrado es que 2^(h-1) 
+	 * sea igual al número de nodos*/
+
+	public static <E> void main(String[] args) {
+
+	}
+
+}
