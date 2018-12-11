ee3f4122c84b320491d1b90cef5f916e6054f5be
Javier Ruiz <javiruizz.jr@gmail.com>
2018-12-11 14:51:51 +0100

    ss

--- P2_3_RuizMuñizJavier/src/Problema1/Ejercicio1.java
+++ P2_3_RuizMuñizJavier/src/Problema1/Ejercicio1.java
@@ -1,30 +1,43 @@
 package Problema1;
 
 import us.lsi.tiposrecursivos.BinaryTree;
+import us.lsi.tiposrecursivos.Tree;
 
 public class Ejercicio1 {
 
 	/* Decidir si un árbol binario es equilibrado */
 	/*
-	 * Un árbol equilibrado es un árbol binario de búsqueda en el que para cada nodo el número de niveles
-	 * de sus subárboles izquierdo y derecho no debe diferir en más de una unidad.
+	 * Un árbol equilibrado es un árbol binario de búsqueda en el que para cada nodo
+	 * el número de niveles de sus subárboles izquierdo y derecho no debe diferir en
+	 * más de una unidad.
 	 */
-	
-	//1.- Crear un árbol binario de búsqueda.
-	/*Sea h el número de niveles, la condición que se debe cumplir para que sea equilibrado es que 2^(h-1) 
-	 * sea igual al número de nodos*/
+
+	// 1.- Crear un árbol binario de búsqueda.
+	/*
+	 * Sea h el número de niveles, la condición que se debe cumplir para que sea
+	 * equilibrado es que 2^(h-1) sea igual al número de nodos
+	 */
 
 	public static <E> void main(String[] args) {
+
+		BinaryTree<Integer> t1 = BinaryTree.leaf(1);
+		BinaryTree<Integer> t2 = BinaryTree.leaf(2);
+		BinaryTree<Integer> t3 = BinaryTree.leaf(3);
+		BinaryTree<Integer> t4 = BinaryTree.leaf(4);
+		BinaryTree<Integer> t6 = BinaryTree.binary(27,BinaryTree.binary(42,t1,t2),BinaryTree.binary(59,t3,t4));
+		System.out.println(t6);
 		
-		System.out.println(esEquilibrado());
+		
+
+		 System.out.println(esAVL(t6));
 
 	}
 
-	private static <E> Boolean esEquilibrado(BinaryTree<E> tree) {
+	public static boolean esAVL(BinaryTree<Integer> t6) {
 		
-		return null;
+		return true;
 	}
-	
-	
+
+
 
 }
